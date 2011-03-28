c3d93e46c04bbca51a4c32102fec92ed070c49b4
kula <kulasama@gmail.com>
2011-03-28 18:45:45 +0800

    first create

--- gurl/__init__.py
+++ gurl/__init__.py
@@ -1,14 +1,21 @@
-from urlparse import urlparse
+from urlparse import urlparse  
+import unittest
 class Url(object):   
     
-    def __init__(self,url):
+    def __init__(self,url):          
         if not url.startswith('http'):
-            url = 'http://'+url
+            url = 'http://'+url 
         self.o = urlparse(url)
+    
+    def __str__(self):
+        return self.url
          
     @property 
-    def domain(self):
-        return self.o.netloc 
+    def domain(self): 
+        _netloc = self.o.netloc
+        array = _netloc.split('.') 
+        _netloc = '.'.join(array[1:])
+        return _netloc
                    
     @property    
     def scheme(self):
@@ -24,12 +31,43 @@ class Url(object):
         
     @property
     def query(self):
-        return self.o.query
+        return self.o.query  
+        
+    @property
+    def port(self):
+        return self.o.port
+        
+    @property
+    def fragment(self):
+        return self.o.fragment 
+    
+    @property
+    def username(self):
+        return self.o.username
+        
+    @property
+    def password(self):
+        return self.o.password
         
         
     @property
     def hostisip(self):
-        return False
+        return False     
+ 
+class gurlTestCase(unittest.TestCase):
+    
+    def testgurl(self):
+        url = Url('http://www.hr125.com/') 
+        self.assertTrue('http://www.hr125.com/',str(url))
+        
+    def testdomain(self):
+        url = Url('http://www.sina.com.cn/test.html')  
+        self.assertEqual(url.domain,'sina.com.cn')
+    
+if __name__ == '__main__':
+    unittest.main()
+        
+
         
         
     
